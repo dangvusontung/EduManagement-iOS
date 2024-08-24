4c063d180557aa10bf3ee134404e0806172d2e3e
TungDVS <dangvusontung@gmail.com>
2024-08-25 01:15:45 +0700

    Initial Commit

---
+++ EduManager/EduManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduManagerApp.swift
+//  EduManager
+//
+//  Created by TungDVS on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct EduManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
